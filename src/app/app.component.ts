172e2de69b2c6916b31d2ceec25a748d4d6e893b
unknown <petrenko1982w@gmail.com>
2020-06-16 17:23:09 +0300

    time is 20:06

---
+++ src/app/app.component.ts
@@ -0,0 +1,22 @@
+import { Component } from '@angular/core';
+
+export interface Todo {
+  id:number
+  title:string
+  completed:Boolean
+  date?:any
+}
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss']
+})
+export class AppComponent {
+  appTitle='Angular todo aplication';
+
+  public todos: Todo[] = [
+    {id: 1, title:"купить хлеб", completed:false, date: new Date()},
+    {id: 2, title:"купить масло", completed:true, date: new Date()},
+    {id: 3, title:"купить пиво", completed:false, date: new Date()},
+  ]
+}
